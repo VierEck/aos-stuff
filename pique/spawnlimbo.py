c84149723dc7005d0daff43bc4132a888718525b
VierEck <viereck.q@gmail.com>
2023-06-26 21:47:13 +0200

    spawnlimbo, fixes & improvements
    -works for green team now too.
    -hover plattform not overlapping map blocks anymore.
    -players spawn at a random location at a fixed range of 64 blocks
     to the tent they chose.
    -fog transitioning back to normal when spawning.

--- pique/spawnlimbo.py
+++ pique/spawnlimbo.py
@@ -13,6 +13,16 @@ from pyspades import world
 from pyspades.packet import register_packet_handler
 from time import time
 import asyncio
+import random
+
+
+
+def send_notice_msg(c):
+	spawn_time = c.spawn_time - time()
+	if spawn_time < 0:
+		spawn_time = 0
+	notice_msg = ("respawn in %.f. left or right to switch between territories. forward to spawn" % spawn_time)
+	c.send_chat_notice(notice_msg)
 
 
 def rotate_dead_pos(c, dir):
@@ -20,7 +30,7 @@ def rotate_dead_pos(c, dir):
 		c.dead_time = time()
 		
 		block_pkt = loaders.BlockAction()
-		block_pkt.player_id = 35
+		block_pkt.player_id = c.player_id
 		block_pkt.value = DESTROY_BLOCK
 		block_pkt.x, block_pkt.y, block_pkt.z = c.dead_pos
 		block_pkt.z += 3
@@ -39,19 +49,28 @@ def rotate_dead_pos(c, dir):
 			if c.current_entity_id >= len(p.entities):
 				c.current_entity_id = 0
 		current_entity = p.entities[c.current_entity_id]
-		c.dead_pos = current_entity.x - 8, current_entity.y, current_entity.z - 8
+		offset = 8
+		if c.team.id == 1:
+			offset = -8
+		x, y, z = current_entity.x - offset, current_entity.y, current_entity.z - 8
+		while p.map.get_solid(x, y, z):
+			z -= 1
+		c.dead_pos = x, y, z
 		
 		pos_pkt = loaders.PositionData()
 		pos_pkt.x, pos_pkt.y, pos_pkt.z = c.dead_pos
 		c.send_contained(pos_pkt)
 		
-		ori_x, ori_y, ori_z = 1.5, 0, 1.5
+		x, y, z = c.dead_pos
+		ori = Vertex3(current_entity.x - x, current_entity.y - y, current_entity.z - z)
+		ori /= ori.length()
+		ori *= 1.5 #zoom effect
 		ori_pkt = loaders.OrientationData()
-		ori_pkt.x, ori_pkt.y, ori_pkt.z = ori_x, ori_y, ori_z
+		ori_pkt.x, ori_pkt.y, ori_pkt.z = ori.x, ori.y, ori.z
 		c.send_contained(ori_pkt)
 		
 		block_pkt = loaders.BlockAction()
-		block_pkt.player_id = 35
+		block_pkt.player_id = c.player_id
 		block_pkt.value = BUILD_BLOCK
 		block_pkt.x, block_pkt.y, block_pkt.z = c.dead_pos
 		block_pkt.z += 3
@@ -67,7 +86,14 @@ async def spawn_limbo(c):
 	while p.entities[c.current_entity_id].team != c.team:
 		c.current_entity_id += 1
 	first_entity = p.entities[c.current_entity_id]
-	c.dead_pos = first_entity.x - 8, first_entity.y, first_entity.z - 8
+	offset = 8
+	if c.team.id == 1:
+		offset = -8
+	x, y, z = first_entity.x - offset, first_entity.y, first_entity.z - 8
+	while p.map.get_solid(x, y, z):
+		z -= 1
+	c.dead_pos = x, y, z
+		
 	
 	spawn_pkt = loaders.CreatePlayer()
 	spawn_pkt.player_id = c.player_id
@@ -78,7 +104,7 @@ async def spawn_limbo(c):
 	c.send_contained(spawn_pkt)
 	
 	block_pkt = loaders.BlockAction()
-	block_pkt.player_id = 35
+	block_pkt.player_id = c.player_id
 	block_pkt.value = BUILD_BLOCK
 	block_pkt.x, block_pkt.y, block_pkt.z = c.dead_pos
 	block_pkt.z += 3
@@ -86,14 +112,19 @@ async def spawn_limbo(c):
 	block_pkt.y -= 1
 	c.send_contained(block_pkt)
 	
-	ori_x, ori_y, ori_z = 1.5, 0, 1.5
+	x, y, z = c.dead_pos
+	ori = Vertex3(first_entity.x - x, first_entity.y - y, first_entity.z - z)
+	ori /= ori.length()
+	ori *= 1.5 #zoom effect
 	ori_pkt = loaders.OrientationData()
-	ori_pkt.x, ori_pkt.y, ori_pkt.z = ori_x, ori_y, ori_z
+	ori_pkt.x, ori_pkt.y, ori_pkt.z = ori.x, ori.y, ori.z
 	c.send_contained(ori_pkt)
 	
 	while True:
 		if c.hp:
 			break
+		
+		send_notice_msg(c)
 		
 		pos_pkt = loaders.PositionData()
 		pos_pkt.x, pos_pkt.y, pos_pkt.z = c.dead_pos
@@ -107,7 +138,7 @@ async def spawn_limbo(c):
 	c.spawn_limbo_loop.cancel()
 	
 	block_pkt = loaders.BlockAction()
-	block_pkt.player_id = 35
+	block_pkt.player_id = c.player_id
 	block_pkt.value = DESTROY_BLOCK
 	block_pkt.x, block_pkt.y, block_pkt.z = c.dead_pos
 	block_pkt.z += 3
@@ -115,15 +146,16 @@ async def spawn_limbo(c):
 	block_pkt.y -= 1
 	c.send_contained(block_pkt)
 	
-	c.spawn_limbo_loop = None
+	c.spawn_limbo_loop = asyncio.ensure_future(live_fog_transition(c))
 	c.dead_pos = None
 	c.dead_time = None
 	c.dead_time_start = None
 	c.current_entity_id = None
 
 
-async def fog_transition(c):
-	r, g, b = c.dead_fog
+async def dead_fog_transition(c):
+	p = c.protocol
+	r, g, b = p.fog_color
 	while True:
 		r += 10
 		g -= 10
@@ -144,28 +176,70 @@ async def fog_transition(c):
 		
 		await asyncio.sleep(1/10)
 	c.spawn_limbo_loop.cancel()
-	c.dead_fog = None
 	c.spawn_limbo_loop = asyncio.ensure_future(spawn_limbo(c))
+
+
+async def live_fog_transition(c):
+	p = c.protocol
+	r, g, b = 255, 100, 100
+	x, y, z = p.fog_color
+	diff_r, diff_g, diff_b = 10, 10, 10
+	if r > x:
+		diff_r = -10
+	if g > y:
+		diff_g = -10
+	if b > z:
+		diff_b = -10
+	while True:
+		r += diff_r
+		g += diff_g
+		b += diff_b
+		
+		if max(r, x) - min(r, x) <= abs(diff_r):
+			r = x
+		if max(g, y) - min(g, y) <= abs(diff_g):
+			g = y
+		if max(b, z) - min(b, z) <= abs(diff_b):
+			b = z
+		
+		fog_pkt = loaders.FogColor()
+		fog_pkt.color = make_color(r, g, b)
+		c.send_contained(fog_pkt)
+		
+		if r == x and g == y and b == z:
+			break
+		
+		await asyncio.sleep(1/10)
+	c.spawn_limbo_loop.cancel()
+	c.spawn_limbo_loop = None
 
 
 def apply_script(protocol, connection, config):
 	
 	class spawn_limbo_c(connection):
-		dead_fog = None
 		dead_pos = None
 		dead_time = None
 		dead_time_start = None
+		spawn_time = None
 		current_entity_id = None
 		allowed_to_spawn = True
 		spawn_limbo_loop = None
 		
 		def on_kill(c, by, kill_type, grenade):
 			p = c.protocol
-			c.dead_fog = p.fog_color
 			c.allowed_to_spawn = False
-			c.send_chat_warning("you are dead. choose a territory to spawn into.")
-			c.spawn_limbo_loop = asyncio.ensure_future(fog_transition(c))
+			c.spawn_time = time() + c.get_respawn_time()
+			send_notice_msg(c)
+			if c.spawn_limbo_loop is not None:
+				c.spawn_limbo_loop.cancel()
+			c.spawn_limbo_loop = asyncio.ensure_future(dead_fog_transition(c))
 			return connection.on_kill(c, by, kill_type, grenade)
+		
+		def on_team_join(c, team):
+			if c.world_object is not None:
+				if not c.hp and c.team.id < 2:
+					return False
+			return connection.on_team_join(c, team)
 		
 		@register_packet_handler(loaders.InputData)
 		def on_input_data_recieved(c, contained: loaders.InputData):
@@ -176,20 +250,25 @@ def apply_script(protocol, connection, config):
 						rotate_dead_pos(c, -1)
 					elif contained.right:
 						rotate_dead_pos(c, 1)
-					elif time() >= c.dead_time_start + p.respawn_time:
+					elif c.allowed_to_spawn:
 						if contained.jump or contained.up or contained.down:
 							x, y, z = c.dead_pos
-							z += 8
+							dir = Vertex3(0, 0, 0)
+							dir.x = random.randrange(0, 100)
+							dir.y = random.randrange(0, 100)
+							dir /= dir.length()
+							dir *= 64
+							x += dir.x
+							y += dir.y
+							z = 60
+							while p.map.get_solid(x, y, z + 1):
+								z -= 1
 							pos = x, y, z
 							c.spawn(pos)
 			return connection.on_input_data_recieved(c, contained)
 		
 		def spawn(c, pos: None = None):
 			if c.allowed_to_spawn:
-				p = c.protocol
-				fog_pkt = loaders.FogColor()
-				fog_pkt.color = make_color(*p.fog_color)
-				c.send_contained(fog_pkt)
 				return connection.spawn(c, pos)
 			else:
 				c.allowed_to_spawn = True
