b12865dd03af7976a8863ae2c4f47d532f83d453
VierEck <85409780+VierEck@users.noreply.github.com>
2024-01-14 21:45:52 +0100

    upload MapTeam.py
    MapTeam.py gives mappers the freedom to choose team names and team colors for their map. 
    a lot of old maps use a green vs blue theme, but not every server follows these color schemes for their team colors by default. 
    so for those servers it may be good if they can change team colors depending on which map is played.

---
+++ pique/MapTeam.py
@@ -0,0 +1,49 @@
+'''
+Easily define Team names and colors per map in the map.txt extension section.
+
+example:
+
+	extensions = {
+		"TeamName1"  : "Pink",
+		"TeamColor1" : (255,   0, 255),
+		"TeamName2"  : "Yellow",
+		"TeamColor2" : (255, 255, 0),
+	}
+
+if one of these extension item is missing it will default to its contemporary value from config.toml
+
+Author: VierEck.
+'''
+
+from piqueserver.config import config
+
+def apply_script(pro, con, cfg):
+
+	class MapTeamColor_P(pro):
+		
+		def on_map_change(p, map_):
+			ext = p.map_info.extensions
+			
+			#Team 1 ("blue")
+			if "TeamColor1" in ext:
+				p.team_1.color = tuple(ext["TeamColor1"])
+			else:
+				p.team_1.color = tuple(config.section('team1').option('color', default=(0, 0, 196)).get())
+			if "TeamName1" in ext:
+				p.team_1.name = str(ext["TeamName1"])
+			else:
+				p.team_1.name = str(config.section('team1').option('name', default="Blue").get())
+			
+			#Team 2 ("green")
+			if "TeamColor2" in ext:
+				p.team_2.color = tuple(ext["TeamColor2"])
+			else:
+				p.team_2.color = tuple(config.section('team2').option('color', default=(0, 0, 196)).get())
+			if "TeamName2" in ext:
+				p.team_2.name = str(ext["TeamName2"])
+			else:
+				p.team_2.name = str(config.section('team2').option('name', default="Green").get())
+			
+			return pro.on_map_change(p, map_)
+	
+	return MapTeamColor_P, con
