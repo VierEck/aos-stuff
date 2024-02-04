8f3102440469d8417ac53da58281150ade08bb02
VierEck <85409780+VierEck@users.noreply.github.com>
2024-02-04 14:01:19 +0100

    NadeLauncher: fixes and credits
    -on_shoot_set only fires up if weapon primary fire state changed. 
     so when u hold shoot it doesnt fire up and no nade spawns. 
     so we fix that by using a loop instead. 
    -credit authors from the other scripts in the author section
    -add link to TeaBoy's pysnip script

--- pique/NadeLauncher.py
+++ pique/NadeLauncher.py
@@ -2,9 +2,10 @@
 explosion on impact with block
 
 this is a barebones supplementory script which doesnt work on its own. modify or create a script extending on it.
-player.has_NadeLauncher must be set to True to if u want a player to be able to use the NadeLauncher. 
+to give player the nadelauncher append them to protocol.NadeLauncher_list
 
-a recreation of the nadelauncher from Teaboy's server. (he didnt release his script or if he did than im not aware of it)
+a recreation of the nadelauncher from Teaboy's pysnip server.
+	https://github.com/TeaBoyy/AoS_TeaBoy_Server/blob/main/scripts/grenade_launchers.py
 some parts of this code r from nadelauncher_ohnoez.py (maintainer: Wizardian):
 	https://github.com/aloha-pk/spades-public/blob/master/legacy/scripts/nadelauncher_ohnoez.py
 some parts of this code r from airstrike2.py by hompy:
@@ -12,11 +13,16 @@ some parts of this code r from airstrike2.py by hompy:
 
 Authors: 
 	VierEck.
+	TeaBoy
+	hompy
+	Wizardian
 '''
 
 
+import asyncio
 from time import monotonic as time
-from pyspades.contained import GrenadePacket
+from pyspades.packet import register_packet_handler
+from pyspades.contained import GrenadePacket, WeaponInput
 from pyspades.constants import UPDATE_FREQUENCY, WEAPON_TOOL
 from pyspades.world import Grenade
 
@@ -25,37 +31,63 @@ from pyspades.world import Grenade
 def apply_script(pro, con, cfg):
 
 	class NadeLauncher_C(con):
-		has_NadeLauncher       = False
 		NadeLauncher_last_time = 0.0
-		NadeLauncher_velocity  = 1.0
+		NadeLauncher_speed     = 1.0
 		
-		def on_shoot_set(c, fire):
-			if c.has_NadeLauncher and fire and c.tool == WEAPON_TOOL:
-				if time() > c.NadeLauncher_last_time + c.weapon_object.delay:
-					p = c.protocol
-					c.NadeLauncher_last_time = time()
-					
-					pos  = c.world_object.position
-					vel  = c.world_object.orientation
-					vel *= c.NadeLauncher_velocity
-					
-					nade = p.world.create_object(Grenade, 0.0, pos, None, vel, c.grenade_exploded)
-					nade.team = c.team
-					
-					collision = nade.get_next_collision(UPDATE_FREQUENCY)
-					if not collision:
-						return
-					eta, x, y, z = collision
-					nade.fuse = eta
-						
-					pkt = GrenadePacket()
-					pkt.player_id = c.player_id
-					pkt.value     = nade.fuse
-					pkt.position  = pos.get()
-					pkt.velocity  = vel.get()
-					p.broadcast_contained(pkt)
-					
-			return con.on_shoot_set(c, fire)
+		def on_spawn(c, pos):
+			c.protocol.NadeLauncher_list.append(c)
+			return con.on_spawn(c, pos)
+		
+		def NadeLauncher_on_nade_exploded(c, nade):
+			c.grenade_exploded(nade)
+		
+		def NadeLauncher_shoot(c):
+			p = c.protocol
+			c.NadeLauncher_last_time = time()
+			
+			pos  = c.world_object.position
+			vel  = c.world_object.orientation
+			vel *= c.NadeLauncher_speed
+			
+			nade = p.world.create_object(Grenade, 0.0, pos, None, vel, c.NadeLauncher_on_nade_exploded)
+			nade.team = c.team
+			
+			collision = nade.get_next_collision(UPDATE_FREQUENCY)
+			if not collision:
+				return
+			eta, x, y, z = collision
+			nade.fuse = eta
+				
+			nade_pkt = GrenadePacket()
+			nade_pkt.player_id = c.player_id
+			nade_pkt.value     = nade.fuse
+			nade_pkt.position  = pos.get()
+			nade_pkt.velocity  = vel.get()
+			p.broadcast_contained(nade_pkt)
 	
 	
-	return pro, NadeLauncher_C
+	class NadeLauncher_P(pro):
+		
+		NadeLauncher_list = []
+		NadeLauncher_bullet_loop_task = None
+		async def NadeLauncher_bullet_loop(p):
+			while True:
+				await asyncio.sleep(0.05) #20 ups
+				for pl in p.NadeLauncher_list:
+					if pl.world_object and not pl.world_object.dead and pl.tool == WEAPON_TOOL:
+						if pl.world_object.primary_fire and time() > pl.NadeLauncher_last_time + pl.weapon_object.delay:
+							pl.NadeLauncher_shoot()
+		
+		def on_map_change(p, map_):
+			if p.NadeLauncher_bullet_loop_task is None:
+				p.NadeLauncher_bullet_loop_task = asyncio.ensure_future(p.NadeLauncher_bullet_loop())
+			return pro.on_map_change(p, map_)
+		
+		def on_map_leave(p):
+			if p.NadeLauncher_bullet_loop_task is not None:
+				p.NadeLauncher_bullet_loop_task.cancel()
+				p.NadeLauncher_bullet_loop_task = None
+			return pro.on_map_leave(p)
+	
+	
+	return NadeLauncher_P, NadeLauncher_C
