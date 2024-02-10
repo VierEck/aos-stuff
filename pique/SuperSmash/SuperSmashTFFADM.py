380559ba93990f97d423a5e018d7be5107cf7e39
VierEck <85409780+VierEck@users.noreply.github.com>
2024-02-10 23:07:56 +0100

    Add SuperSmashTFFADM.py

---
+++ pique/SuperSmash/SuperSmashTFFADM.py
@@ -0,0 +1,223 @@
+'''
+SuperSmashOff FreeForAll Timed DeathMatch. Gamemode script.
+
+Player with most kills at round end wins.
+Players r all in the same team so they see each other on the map.
+Your companions r spawned on the opposite team to visually distinguish them from enemies.
+
+recommended setting: (since a game lasts the whole duration of map time limit)
+	default_time_limit = 10min
+
+pls include disco.py for disco effect at round end
+
+
+based on FreeForAll by Yourself:
+	https://github.com/aloha-pk/piqueserver/blob/master/piqueserver/game_modes/freeforall.py
+
+Authors: 
+	VierEck.
+	Yourself
+'''
+
+
+from random import randint
+from twisted.internet.reactor import callLater
+from pyspades.constants import CTF_MODE
+from piqueserver.config import config
+from piqueserver.commands import command, target_player
+from pyspades.contained import CreatePlayer, IntelCapture, PositionData
+from pyspades.world import Grenade
+from pyspades.common import Vertex3
+
+
+smash_cfg = config.section("SuperSmashOff")
+PLAYER_TEAM = smash_cfg.option("SSTFFADM_player_team", None).get() #0 == team1/blue; 1 == team2/green
+
+
+def print_scores(c):
+	msg = ""
+	msg += " | kills: +"    + str(pl.smash_kills)
+	msg += " | deaths: -"   + str(pl.smash_deaths)
+	msg += " | suicides: -" + str(pl.smash_suicides)
+	msg += " | overall: "   + str(pl.smash_get_score())
+	return msg
+
+
+@command('smash_score')
+@target_player
+def smash_get_scores(c, pl):
+	pl.send_chat(pl.name + print_scores(pl))
+
+
+#
+def apply_script(pro, con, cfg):
+
+	class SuperSmashTFFADM_C(con):
+		smash_kills    = 0
+		smash_deaths   = 0
+		smash_suicides = 0
+		
+		def smash_get_score(c):
+			return c.smash_kills - c.smash_deaths - c.smash_suicides
+	
+		def on_team_join(c, team):
+			if not team.spectator:
+				return PLAYER_TEAM
+			return con.on_team_join(c, team)
+		
+		def on_spawn(c, pos):
+			if c.local:
+				b = c
+				try:
+					bot_team = b.smash_bot_friend.team.other
+					
+					create_pkt = CreatePlayer()
+					create_pkt.x, create_pkt.y, create_pkt.z = pos
+					create_pkt.weapon    = b.weapon
+					create_pkt.player_id = b.player_id
+					create_pkt.name      = b.name
+					create_pkt.team      = bot_team.id
+					
+					b.smash_bot_friend.send_contained(create_pkt)
+				except AttributeError:
+					pass
+			return con.on_spawn(c, pos)
+		
+		def respawn(c):
+			p = c.protocol
+			x, y, z = p.get_random_location(True)
+			z -= 2
+			pos = x, y, z
+			t = c.get_respawn_time() - 3
+			def foresee_spawn(c):
+				pos_pkt = PositionData()
+				pos_pkt.x, pos_pkt.y, pos_pkt.z = pos
+				c.send_contained(pos_pkt)
+			if t > 0.5:
+				callLater(t, foresee_spawn, c)
+			else:
+				foresee_spawn(c)
+			if c.spawn_call is None:
+				c.spawn_call = callLater(c.get_respawn_time(), c.spawn, pos)
+			#hijack
+		
+		def on_kill(c, killer, kill_type, nade):
+			p = c.protocol
+			if p.smash_round_end:
+				return False
+			if killer and c != killer:
+				killer.smash_kills += 1
+				c.smash_deaths     += 1
+				
+				killer.streak += 1
+				killer.best_streak = max(killer.streak, killer.best_streak)
+			else:
+				c.smash_suicides += 1
+			return con.on_kill(c, killer, kill_type, nade)
+	
+	
+	class SuperSmashTFFADM_P(pro):
+		game_mode       = CTF_MODE
+		smash_round_end = False
+		
+		def on_game_end(p):
+			p.smash_round_end = True
+			score_player_list = []
+			x, y, z = 256, 256, 0
+			for pl in p.players.values():
+				score = pl.smash_get_score()
+				
+				did_insert = False
+				for i in range(score_player_list):
+					other = score_player_list[i]
+					other_score = other.smash_get_score()
+					if score > other_score:
+						score_player_list.insert(i, pl)
+						did_insert = True
+						break
+				if not did_insert:
+					score_player_list.append(pl)
+						
+				if not pl.team.spectator:
+					#gather everyone in map center
+					pos = x + randint(-5, 5), y + randint(-5, 5), z
+					pl.spawn(pos)
+			
+			if len(score_player_list) > 0:
+				winner = score_player_list[0]
+				
+				placement_list = []
+				placement      = 0
+				last_score     = -1234567 #
+				for i in range(0, len(score_player_list)):
+					pl = score_player_list[i]
+					score = pl.smash_get_score()
+					if last_score != score:
+						last_score = score
+						placement += 1
+					placement_list.append(placement)
+				
+				#anounce podium
+				p.broadcast_chat_status(winner.name + " has won!")
+				msg = ""
+				for i in range(min(5, len(score_player_list))):
+					if placement_list[i] > 3:
+						break
+					pl = score_player_list[i]
+					msg += str(placement_list[i]) + ". " + str(pl.name)
+					msg += print_scores(pl) + '\n'
+				p.broadcast_chat(msg)
+				
+				#inform about ur own stats
+				for i in len(score_player_list):
+					pl = score_player_list[i]
+					msg = ""
+					msg += str(placement_list[i]) + ". " + str(pl.name)
+					msg += print_scores(pl)
+					pl.send_chat(msg)
+				
+				#play the winning fanfare
+				intel_pkt = IntelCapture()
+				intel_pkt.player_id = winner.player_id
+				intel_pkt.winning = True
+				self.protocol.broadcast_contained(intel_pkt)
+				
+				#one last bang. fling everyone in random directions
+				vPos = Vertex3(x, y, z + 5)
+				vel  = Vertex3()
+				nade = p.world.create_object(Grenade, 0.0, vPos, None, vel, winner.grenade_exploded)
+				nade_pkt = GrenadePacket()
+				nade_pkt.player_id = c.player_id
+				nade_pkt.value     = nade.fuse
+				nade_pkt.position  = vPos.get()
+				nade_pkt.velocity  = vel.get()
+				p.broadcast_contained(nade_pkt)
+			pro.on_game_end(p) #disco
+		
+		def _time_up(p):
+			p.on_game_end()
+			pro._time_up(p)
+		
+		def on_map_change(p, map_):
+			p.max_score       = 1
+			p.smash_round_end = False
+			p.friendly_fire   = True
+			ext = p.map_info.extensions
+			global PLAYER_TEAM
+			if PLAYER_TEAM is None:
+				if "SSTFFADMPlayerTeam" in ext and ext["SSTFFADMPlayerTeam"] == 0:
+					PLAYER_TEAM = p.team_1
+				else:
+					PLAYER_TEAM = p.team_2
+			elif PLAYER_TEAM == 0:
+				PLAYER_TEAM = p.team_1
+			else:
+				#by default 2, since team_2 on aloha is red and red is cooler than blue
+				PLAYER_TEAM = p.team_2
+			return pro.on_map_change(p, map_)
+		
+		def get_mode_name(p): #server list
+			return "SSTFFADM"
+	
+	
+	return SuperSmashTFFADM_P, SuperSmashTFFADM_C
