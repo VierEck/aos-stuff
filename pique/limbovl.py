14acc8bea9533f6c74e2e7f9ae73ddc3d641ff5f
VierEck <85409780+VierEck@users.noreply.github.com>
2024-02-26 21:32:54 +0100

    exovl: rename to limbovl

--- pique/limbovl.py
+++ pique/limbovl.py
@@ -1,5 +1,5 @@
 '''
-ExOVL. Secret Spectating from limbo.
+LimbOVL. Secret Spectating from limbo.
 
 
 Authors: 
@@ -22,7 +22,7 @@ def notification(c, msg):
 	p.irc_say("* " + msg)
 
 
-def do_exovl(pl):
+def do_limbovl(pl):
 	create_pkt = CreatePlayer()
 	create_pkt.player_id = pl.player_id
 	create_pkt.name      = "secret Deuce"
@@ -30,42 +30,42 @@ def do_exovl(pl):
 	create_pkt.team      = -1
 	create_pkt.x, create_pkt.y, create_pkt.z = 256, 256, 0
 	pl.send_contained(create_pkt)
-	notification(pl, "you are now using exovl")
+	notification(pl, "you are now using limbovl")
 
 
-@command("exovl", admin_only=True)
-def exovl(c, ip):
+@command("limbovl", admin_only=True)
+def limbovl(c, ip):
 	p = c.protocol
 	ip = ip_address(str(ip))
 	for pl in p.connections.values():
 		if pl.name is None and ip == ip_address(pl.address[0]):
-			do_exovl(pl)
+			do_limbovl(pl)
 			return
-	p.exovl_ip_list.append(ip)
+	p.limbovl_ip_list.append(ip)
 	def remove_ip():
-		if ip in p.exovl_ip_list:
-			p.exovl_ip_list.remove(ip)
+		if ip in p.limbovl_ip_list:
+			p.limbovl_ip_list.remove(ip)
 	callLater(300, remove_ip)
-	notification(c, "ip %s marked for exovl. you have 5min to connect" % ip)
+	notification(c, "ip %s marked for limbovl. you have 5min to connect" % ip)
 
 
 def apply_script(pro, con, cfg):
 	
 	
-	class exovl_C(con):
+	class limbovl_C(con):
 		
 		def on_join(c) -> None:
 			p = c.protocol
-			if ip_address(c.address[0]) in p.exovl_ip_list:
+			if ip_address(c.address[0]) in p.limbovl_ip_list:
 				def send_a_bit_later():
-					p.exovl_ip_list.remove(ip_address(c.address[0]))
-					do_exovl(c)
+					p.limbovl_ip_list.remove(ip_address(c.address[0]))
+					do_limbovl(c)
 				callLater(1, send_a_bit_later)
 			return con.on_join(c)
 	
 	
-	class exovl_P(pro):
-		exovl_ip_list = []
+	class limbovl_P(pro):
+		limbovl_ip_list = []
 	
 	
-	return exovl_P, exovl_C
+	return limbovl_P, limbovl_C
